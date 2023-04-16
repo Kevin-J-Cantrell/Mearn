6a34066711936985b7eb5466baead0b0a85ed67b
Kevin J Cantrell <112343044+Kevin-J-Cantrell@users.noreply.github.com>
2023-04-15 18:20:03 -0700

    commiting products

---
+++ Week-6/Product_Manager_part-3/server/routes/product.routes.js
@@ -0,0 +1,9 @@
+const ProductController = require('../controllers/product.controller'); // rename to ProductController
+
+module.exports = app => {
+    app.get('/api/Products', ProductController.findAllProducts);
+    app.post('/api/Products', ProductController.createNewProduct);
+    app.get('/api/Products/:id', ProductController.findOneProduct);
+    app.put('/api/Products/:id', ProductController.updateExistingProduct);
+    app.delete('/api/Products/:id', ProductController.deleteAnExistingProduct);
+}
